790b11abd5d376da44ca2fbbbcfef3c0c3b01349
jenja-pa <jenja.pa@gmail.com>
2022-10-13 17:45:51 +0300

    Correct duplicate print and use buildin max and min

--- HT_3/task_6.py
+++ HT_3/task_6.py
@@ -1,26 +1,16 @@
 # task_6.py
 # Write a script to get the maximum and minimum VALUE in a dictionary.
 # 
+# # Робота над помилками:
+# * замінено два принти на один, з f-рядком
+# * дійсно в попередньому варіанті забув, що існують спеціальні 
+# вбудовані функції пошуку max та min
 
 dct = {1: 1, 2:2, 3:1, 4:1, 5:2, 6:3, 7:1, 8:4, 9:2}
-print("Input:")
-print(f"{dct=}")
-print(" values:", end=" ")
-print(", ".join(map(str, dct.values())))
-
-max_val = None
-for el in dct.values():
-    max_val = el
-    break
-min_val = max_val
-
-for val in dct.values():
-    if max_val < val:
-        max_val = val
-    if min_val > val:
-        min_val = val
+print(f"Input: {dct=}")
+print(f" values: {', '.join(map(str, dct.values()))}")
 
 print("-"*78)
-print(f"Max value of dict: {max_val}")
-print(f"Min value of dict: {min_val}")
+print(f"Max value of dict: {max(dct.values())}")
+print(f"Min value of dict: {min(dct.values())}")
 
