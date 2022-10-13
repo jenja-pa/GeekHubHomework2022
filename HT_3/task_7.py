ae2e3bb84602ea51aa3f21d95ba981b448856e85
jenja-pa <jenja.pa@gmail.com>
2022-10-13 17:54:22 +0300

    Correct duplicate print and срфтпу use pow to multiplicaешщт

--- HT_3/task_7.py
+++ HT_3/task_7.py
@@ -1,7 +1,14 @@
 # task_7.py
-# Write a script which accepts a (int) from user and generates dictionary in range where key is and value is
+# Write a script which accepts a (int) from user and generates 
+#  dictionary in range where key is and value is
 #  * e.g. 3 --> {0: 0, 1: 1, 2: 4, 3: 9}
-
+#  
+# # Робота над помилками:
+# * замінено два принти на один, з f-рядком
+# * зкориговано = int(idx ** 2), на = idx * idx, приведення типу було 
+# потрібне оскільки піднесення у степінь повертає float, але в даному 
+# випадку ця операція дійсно непідходящий варіант використання
+# 
 dct = dict()
 
 try:
@@ -9,10 +16,9 @@ try:
     if n < 0:
         raise ValueError
     for idx in range(n + 1):
-        dct[idx] = int(idx ** 2)
+        dct[idx] = idx * idx
     print("-"*78)
-    print("Result")
-    print(f"{dct}")
+    print(f"Result: {dct}")
 except ValueError as e:
     print("Error. Please next enter a correct number.")
 
