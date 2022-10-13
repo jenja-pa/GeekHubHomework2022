0114aa769a8272b8f697fbfd214e7fdeb1d4ae04
jenja-pa <jenja.pa@gmail.com>
2022-10-13 17:36:35 +0300

    Correct duplicate print and modi determine unique values

--- HT_3/task_5.py
+++ HT_3/task_5.py
@@ -1,18 +1,13 @@
 # task_5.py
 # Write a script to remove values duplicates from dictionary. Feel free to hardcode your dictionary.
 # 
+# # Робота над помилками:
+# * замінено два принти на один, з f-рядком
+# * попередній варант пердбачав повернення словника з вилученими елементами значення яких
+# дублюється, а потрібно було отримати тільки унікальні значення.
 
 dct = {1: 1, 2:2, 3:1, 4:1, 5:2, 6:3, 7:1, 8:4, 9:2}
-print("Input:")
-print(f"{dct=}")
-
-lst = []
-dct_result = dict()
-for key, val in dct.items():
-    if not val in lst:
-        lst.append(val)
-        dct_result[key] = val
-
+print(f"Input dict: {dct}")
+print(f"Input values: {tuple(dct.values())}")
 print("-"*78)
-print("After delete duplicate values:")
-print(f"{dct_result=}")
+print(f"Unique values: {tuple(set(dct.values()))}")
