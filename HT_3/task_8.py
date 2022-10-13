8ffd3edf7edd9c5fb86a367f55778a4249241303
jenja-pa <jenja.pa@gmail.com>
2022-10-13 18:05:33 +0300

    Fixed limitation on the minimum value entered by the user

--- HT_3/task_8.py
+++ HT_3/task_8.py
@@ -1,14 +1,18 @@
 # task_8.py
 # Створити цикл від 0 до ... (вводиться користувачем). 
-# В циклі створити умову, яка буде виводити поточне значення, якщо остача від ділення на 17 дорівнює 0.
+# В циклі створити умову, яка буде виводити поточне значення, якщо 
+# остача від ділення на 17 дорівнює 0.
 # 
+# Робота над помилками:
+# * Видалено одне із обмеженнь на мінімальне значення, що може ввести
+# користувач
 
 try:
-    n = int(input("Enter positive integer number larger 17: "))
-    if n < 0 or n < 17:
+    n = int(input("Enter positive integer number: "))
+    if n < 0 :
         raise ValueError
 
-    print("Numbers which is shared entirely by 17")
+    print(f"Numbers which is shared entirely by 17 in range 0 .. {n}")
     for n in range(n + 1):
         if n % 17 == 0:
             print(n)
