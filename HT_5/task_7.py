505daf52cad19a82ff49595492c2e33a4532677e
jenja-pa <jenja.pa@gmail.com>
2022-10-26 20:33:07 +0300

    task 7 - Append case: Count equaly items in list

--- HT_5/task_7.py
+++ HT_5/task_7.py
@@ -57,6 +57,48 @@ def count_els(s_value):
     print(", ".join(out_lst))
 
 
+def cnt_eq_list_items(seq):
+    """
+    Підрахунок кількості однакових елементів у списку
+    В якості елементів можуть бути будь які (змінні та незмінні типи)
+    Реалізувати такий підрахунок за допомогою словника неможливо, 
+     набір (set) також застосувати неможливо
+    """
+    def get_idx(item, lst):
+        """
+        пошук індекса у списку елементів виду [[value, cnt], ... ] по значенню value
+
+        return
+            індекс елемента якщо його знайдено 
+            -1 якщо елемен не знайдено
+        """
+
+        for idx, value in enumerate(lst):
+            if value[0] == item and type(value[0]) == type(item):
+                return idx
+
+        return -1
+
+    result = []
+    for el in seq:
+        idx = get_idx(el, result)
+        if idx == -1: 
+            # Not found
+            result.append([el, 1])
+        else:
+            # Found
+            result[idx][1] += 1
+
+    return result
+
+def help_quote(value):
+    return "'" if isinstance(value, str) else ""
+
 if __name__ == "__main__":
+    # case 1 - String
     count_els("1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2]") # ----> "1 -> 3, foo -> 2, [1, 2] -> 2, True -> 1"
-    
+    # case 2- List
+    output = []
+    for item in cnt_eq_list_items([1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2]]):
+        output.append(f"{help_quote(item[0])}{item[0]}{help_quote(item[0])} -> {item[1]}")
+    print(", ".join(output))
