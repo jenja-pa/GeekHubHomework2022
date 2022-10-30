7a68bdbc0f61c83a2c43fc096617ebbb05a247dc
jenja-pa <jenja.pa@gmail.com>
2022-10-30 22:48:00 +0200

    task 1 - Traffic ligts - infimite loop

--- HT_8/task_1.py
+++ HT_8/task_1.py
@@ -26,62 +26,41 @@
       # Red        Green
 from time import sleep
 import sys
+import os
+import shutil
 
-def traffic_lights(seconds=25):
-    """
-    Емуляція світлофора для авто і пішоходів
+def my_align(value, width):
+    spaces = width - len(str(value))
+    return str(value) + " " * spaces
 
-    Кожну секунду виводити поточний стан світлофора для авто та пішоходів
+# Список станів (авто., піш., час дії)
+lst_states = [
+    ("Red", "Green", 4),
+    ("Yellow", "Red", 2),
+    ("Green", "Red", 4) 
+]
+CNT_STATES = len(lst_states)
 
-    Вхідні дані:
-     - seconds - кількість секунд емуляції, 
-        якщо None - безкінечний цикл
-    Вихідні дані:
-        Вивід стану не консоль
-    """
+os.system('cls' if os.name=='nt' else 'clear')
 
-    # Список станів (авто., піш., час дії)
-    lst_states = [
-        ("red", "green", 4),
-        ("yellow", "red", 2),
-        ("green", "red", 4) 
-    ]
-    CNT_STATES = len(lst_states)
-    
-    # test parameter
-    if not isinstance(seconds, int):
-        raise TypeError("Your seconds parameter must be int")
-    seconds = int(seconds)
-    if seconds < 3:
-        raise ValueError("Your parameter seconds must be 3 or more")
+width, _ = shutil.get_terminal_size()
+width_2 = width // 2
 
-    phase = 0
-    len_time = len(str(seconds))
-    cur_tick = lst_states[phase][2]
-    print(f"n tick|{'Auto':<15} {'Pedestrian':>15}")
-    for sec in range(seconds):
-        print(f"{sec:>6}|{lst_states[phase][0]:<15} {lst_states[phase][1]:>15}")
+
+phase = 0
+cur_tick = lst_states[phase][2]
+while True:
+# for sec in ange(seconds):
+    try:
+        print(f"{my_align(lst_states[phase][0], width_2)}{my_align(lst_states[phase][1], width_2)}")
         sleep(1)
         cur_tick -= 1
         if cur_tick == 0:
             phase = (phase + 1) % CNT_STATES
-            cur_tick = lst_states[phase][2]
-
-
-
-if __name__ == "__main__":
-    seconds = 25
-    for attempt in range(5):
-        try:
-            tmp = input("Enter count of time in second(int) for emulation (def. 25)")
-            seconds = int(tmp)
-        except ValueError as ex: 
-            if len(tmp) == 0:
-                break
-            continue
+            cur_tick = lst_states[phase][2]        
+    except KeyboardInterrupt as ex:
         break
 
-    traffic_lights(seconds)
 
 
 
