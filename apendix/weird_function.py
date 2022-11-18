f33c1f0101702aea84a1fad9c54a3b7773565294
Panasko E.V <jenja.pa@gmail.com>
2022-11-18 15:31:29 +0200

    Фіксація додаткової задачі - super_weird_sum, 4,5,6 варіант

--- apendix/weird_function.py
+++ apendix/weird_function.py
@@ -5,6 +5,7 @@
 #     print(super_weird_sum(5)(3)())        -> 8
 #     print(super_weird_sum(5)(4)(-10)())   -> -1
 
+# 1
 # def super_weird_sum(value):
 #     inner_lst = [value]
 
@@ -16,6 +17,7 @@
 #             return sum(inner_lst)
 #     return inner_fun    
 
+# 2 Без if
 # def super_weird_sum(value):
 #     inner_lst = [value]
 
@@ -26,21 +28,71 @@
 
 #     return inner_fun    
 
+# 3 без списка, але із nonlocal
+# def super_weird_sum(value):
+#     inner_sum = value
+
+#     def inner_fun(arg=None):
+#         nonlocal inner_sum
+#         cases = {None: inner_sum}
+#         inner_sum += arg or 0
+#         return cases.get(arg, inner_fun)
+
+#     return inner_fun    
+
+# # 4 - без nonlocal, functools.partial
+# from functools import partial
+
+
+# def super_weird_sum(value):
+
+#     def inner_fun(arg=None, keep_sum=0):
+#         keep_sum += arg or 0
+#         cases = {None: keep_sum}
+#         return cases.get(arg, partial(inner_fun, keep_sum=keep_sum))
+
+#     return partial(inner_fun, keep_sum=value)    
+
+# 5 - partial без functools
+# def super_weird_sum(value):
+
+#     def partial(func, *part_args):
+#         def wrapper(*extra_args):
+#             args = list(part_args)
+#             args.extend(extra_args)
+#             return func(*args) if extra_args else part_args[0]
+
+#         return wrapper
+
+#     def inner_fun(*args):
+#         return partial(inner_fun, sum(args))
+
+#     return partial(inner_fun, value)    
+
+# 6 - partial без functools, тільки 1 аргумент
 def super_weird_sum(value):
-    inner_sum = value
 
-    def inner_fun(arg=None):
-        nonlocal inner_sum
-        cases = {None: inner_sum}
-        inner_sum += arg or 0
-        return cases.get(arg, inner_fun)
+    def partial(func, *part_args):
+        def wrapper(*extra_args):
+            args = list(part_args)
+            if extra_args:
+                args.extend([extra_args[0]])
+                return func(*args)
+            return part_args[0]
 
-    return inner_fun    
+        return wrapper
+
+    def inner_fun(*args):
+        return partial(inner_fun, sum(args))
+
+    return partial(inner_fun, value)    
+
 
 if __name__ == "__main__":
+    print(f"super_weird_sum(5)(4)(-10)() = {super_weird_sum(5)(4)(-10)()}")
     print(f"super_weird_sum(5)() = {super_weird_sum(5)()}")
     print(f"super_weird_sum(5)(3)() = {super_weird_sum(5)(3)()}")
-    print(f"super_weird_sum(5)(4)(-10)() = {super_weird_sum(5)(4)(-10)()}")
+    print(f"super_weird_sum(5)(3, 1000)() = {super_weird_sum(5)(3, 1000)()}")
     print(f"super_weird_sum(5)(4)(-10)(11)() = "
           f"{super_weird_sum(5)(4)(-10)(11)()}")
 
