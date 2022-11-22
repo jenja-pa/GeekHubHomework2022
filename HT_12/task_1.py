87102954be7541c51d4ce72169a7b9839daa6e51
Panasko E.V <jenja.pa@gmail.com>
2022-11-22 10:45:10 +0200

    Виправлення task_1 - Властивість color перенесено в атрибути базового класу

--- HT_12/task_1.py
+++ HT_12/task_1.py
@@ -7,17 +7,18 @@
 # їх створенні.
 
 class Figure:
-    def __init__(self, color="White"):
-        self.color = color
+    color = "White"
 
-    def set_color(self, color):
-        self.color = color
+    @classmethod
+    def set_color(cls, color):
+        print(f"set_color: {cls}")
+        cls.color = color
   
     def __str__(self):
-        return f"This is Figure(color={self.color})"
+        return f"This is Figure() cls.color={self.color})"
 
     def __repr__(self):
-        return f"Figure(color={self.color})"
+        return "Figure()"
 
 
 class Oval(Figure):
@@ -48,25 +49,32 @@ class Square(Figure):
         return f"Square({self.width}, {self.height})"
 
 
-print("Figure")
+print("Create Figure")
 f1 = Figure()
-print(f"{f1=}")
-print("Figure Green")
-f2 = Figure("Green")
-print(f"{f2=}")
-
+print(f"{str(f1)}")
+print("Figure change color to Green")
+f2 = Figure()
+f2.set_color("Green")
+print(f"{str(f2)}")
 print()
+
+print("Create Oval o1")
 o1 = Oval(23, 45)
-print(o1)
+print(f"o1={str(o1)}")
 print("Change color: to Red")
 o1.set_color("Red")
-print(o1)
-print(f"{o1=}")
-
+print(f"o1={str(o1)}")
+print("Create Another Oval o2, color steel Red")
+o2 = Oval(2, 4)
+print(f"o2={str(o2)}")
 print()
+
+print("Create Square s1")
 s1 = Square(12)
-print(f"{s1=}")
+print(f"s1={str(s1)}")
 print("Change color: to Blue")
 s1.set_color("Blue")
-print(s1)
-print(f"{s1=}")
+print(f"s1={str(s1)}")
+print("Create Another Square s2")
+s2 = Square(2)
+print(f"s2={str(s2)}")
