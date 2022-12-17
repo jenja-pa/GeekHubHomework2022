ae38d139f2d1d86a452d274036bf7233f880333e
jenja-pa <jenja.pa@gmail.com>
2022-12-17 15:54:36 +0200

    Створення робочого паука - отримання інформації від webstore extention

--- HT_16/scrapy_crawlers/scrapy_crawlers/spiders/chrome_webstore.py
+++ HT_16/scrapy_crawlers/scrapy_crawlers/spiders/chrome_webstore.py
@@ -32,29 +32,58 @@ class ChromeWebstoreSpider(scrapy.Spider):
         self.log("parse_sitemap = 1")
 
         lst_ns = ChromeWebstoreSpider.get_namespaces(response)
-        self.log(f"{lst_ns=}")
+        self.log(f"Response namespaces: {lst_ns=}")
         for name_ns, url_ns in lst_ns:
             response.selector.register_namespace('d' if name_ns == "" else name_ns, url_ns)
         # lst = response.xpath("//*[name()='loc']/text()").getall()
         # self.log(f'{lst=}')
-        lst = response.xpath("//d:loc/text()").getall()
-        self.log(f'{lst=}')
+        lst_locs = response.xpath("//d:loc/text()").getall()
+        # self.log(f'{lst=}')
+        # todo debug feature - save response
         # filename = "2_response_loc.txt"
         # with open(filename, "w") as file:
         #     for loc in lst:
         #         file.write(f"{loc}\n")
         # self.log(f'Saved file {filename}')
-        for idx, url_next_page in enumerate(lst):
+        for idx, url_next_page in enumerate(lst_locs):
             # todo - debug exit
             if idx > 2:
                 return
             yield scrapy.Request(url_next_page, callback=self.parse_shard, cb_kwargs={"n_page": idx})
 
     def parse_shard(self, response, n_page):
-        self.log("parse_shard = 2")
-        filename = f"3_shard_loc_{n_page}.txt"
-        with open(filename, "w") as file:
-            file.write(response.text)
-        self.log(f'Saved file {filename}')
-        pass
+        self.log(f"parse_shard: {n_page}")
+    
+        # todo debug feature - save response
+        # filename = f"3_shard_loc_{n_page}.txt"
+        # with open(filename, "w") as file:
+        #     file.write(response.text)
+        # self.log(f'Saved file {filename}')
 
+        lst_ns = ChromeWebstoreSpider.get_namespaces(response)
+        for name_ns, url_ns in lst_ns:
+            response.selector.register_namespace('d' if name_ns == "" else name_ns, url_ns)
+    
+        lst_locs_webstore = response.xpath("//d:loc/text()").getall()
+        for idx, url_target_page in enumerate(lst_locs_webstore):
+            # todo - debug exit
+            if idx > 1:
+                return
+            yield scrapy.Request(url_target_page, callback=self.parse_page_webstore, cb_kwargs={"n_page": n_page, "idx_in_page": idx})
+    
+    def parse_page_webstore(self, response, n_page, idx_in_page):
+        self.log(f"parse_page_webstore: {n_page=}, {idx_in_page=}")
+
+        # todo debug feature - save response
+        # filename = f"4_p_webstore_{n_page}_{idx_in_page}.txt"
+        # with open(filename, "w", encoding="utf-8") as file:
+        #     file.write(response.text)
+        # self.log(f'Saved file {filename}')
+
+        # todo try get data directly
+        yield ExtentionItem(
+            id_item = response.url.split('/')[-1],
+            name = response.css('h1.e-f-w::text').get(),
+            description = response.xpath('//div[@itemprop="description"]/text()').get()
+            )
+        
