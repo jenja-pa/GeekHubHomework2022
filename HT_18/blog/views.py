41a9a394718fb4c7807c1dec17028ebee6cd4078
jenja-pa <jenja.pa@gmail.com>
2023-01-10 01:42:35 +0200

    Заключні кроки туторіала по джанго

--- HT_18/blog/views.py
+++ HT_18/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
 from django.utils import timezone
+
 from .models import Post
 from .forms import PostForm
 
@@ -19,10 +21,26 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            me = User.objects.get(username=request.user.username)
+            post.author = me
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
